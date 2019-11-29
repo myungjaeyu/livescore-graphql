c2349e2a1e83805c359c6af204ce6396337d5048
u4bi <rootcode10@gmail.com>
2019-11-30 01:57:52 +0900

    feat: Add ring reload middleware.

--- src/livescore_graphql/core.clj
+++ src/livescore_graphql/core.clj
@@ -2,7 +2,8 @@
   (:use org.httpkit.server)
   (:require [compojure.handler :refer [api]]
             [ring.middleware.json :refer [wrap-json-params wrap-json-response]]
-            [livescore-graphql.routes :refer [routes]])
+            [livescore-graphql.routes :refer [routes]]
+            [ring.middleware.reload :refer [wrap-reload]])
   (:gen-class))
 
 (def app
@@ -11,5 +12,5 @@
       wrap-json-response))
 
 (defn -main []
-  (run-server app {:port 3000})
+  (run-server (wrap-reload #'app) {:port 3000})
   (println "livescore-graphql"))
