9e6030c5c103b95e12a6000bf9fafd83f7483ba8
u4bi <rootcode10@gmail.com>
2019-11-30 01:51:08 +0900

    chore: Add ring, ring/ring-json, javax.servlet/servlet-api.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [http-kit "2.3.0"]
-                 [compojure "1.6.1"]])
+                 [compojure "1.6.1"]
+                 [ring "1.7.1"]
+                 [ring/ring-json "0.5.0"]]
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
