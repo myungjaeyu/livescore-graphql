32d842041aa9b8f4c071ef7ae957dd1d6454a1ff
u4bi <rootcode10@gmail.com>
2019-11-30 01:49:31 +0900

    chore: Add http-kit, compojure, org.clojure/data.json.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject livescore-graphql "0.1.0-SNAPSHOT"
   :main livescore-graphql.core
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [http-kit "2.3.0"]
+                 [compojure "1.6.1"]])
